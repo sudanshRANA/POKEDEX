96c585514cce17c6685b260829a2c2311bd8f7eb
SudanshRana <sudanshrana4034@gmail.com>
2025-09-06 11:34:05 +0530

    final responsive done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './App.css'
 import CustomRoutes from './routes/CustomRoutes';
 
 function App() {
+  const navigate = useNavigate();
 
   return (
     <div className="outer-pokedex">
-      <h1 id="pokedex-heading">
-        <Link to="/">Pokedex</Link>
+      <h1 onClick={() => navigate('/')} id="pokedex-heading">
+        PokeDex
       </h1>
       <CustomRoutes />
     </div>
